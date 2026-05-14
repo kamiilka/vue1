7f5d96905db4af2d3b280f8e96ba9d23f24ed99f
kamiilka <kkstr290107@gmail.com>
2026-05-14 14:02:06 +0300

    page1

---
+++ src/components/page1.vue
@@ -0,0 +1,13 @@
+ 
+<template>
+  <div class="page1">
+    <h1>Page 1</h1>
+    <p>This is the content of Page 1.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Page1'
+}                  
+</script>   
